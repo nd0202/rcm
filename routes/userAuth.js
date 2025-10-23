9a0160ead28d64339c38163bb24bb20766f04d0a
nd0202 <K9130655913d@gmail.com>
2025-10-23 21:29:02 +0530

    up6

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -3,10 +3,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import { User } from "../models/user.js";
-
-
-
+import { User } from "../models/user";
 
 
 
