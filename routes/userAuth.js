b421f224ca4f5cd309cb8a725e3f6ee951ce2042
nd0202 <K9130655913d@gmail.com>
2025-10-23 20:05:35 +0530

    up1

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,77 +1,11 @@
-// import express from "express";
-// import bcrypt from "bcryptjs";
-// import jwt from "jsonwebtoken";
-// import dotenv from "dotenv";
-// import { User } from "../models/user.js";
-
-
-// dotenv.config();
-// const authRouter = express.Router();
-
-// // Sign Up
-// authRouter.post("/signup", async (req, res) => {
-//   try {
-//     const { name, email, password } = req.body;
-//     const exist = await User.findOne({ email });
-//     if (exist) return res.status(400).json({ error: "Email already exists" });
-
-//     const hashed = await bcrypt.hash(password, 10);
-//     const user = await User.create({ name, email, password: hashed });
-//     res.json({ message: "Signup success", user });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: "Signup failed" });
-//   }
-// });
-
-// // Login
-// authRouter.post("/login", async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     const user = await User.findOne({ email });
-//     if (!user) return res.status(404).json({ error: "User not found" });
-
-//     const match = await bcrypt.compare(password, user.password);
-//     if (!match) return res.status(401).json({ error: "Invalid credentials" });
-
-//     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
-//     res.json({ token, user });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: "Login failed" });
-//   }
-// });
-
-// // Get Profile
-// authRouter.get("/profile", async (req, res) => {
-//   try {
-//     const token = req.headers.authorization?.split(" ")[1];
-//     if (!token) return res.status(401).json({ error: "Unauthorized" });
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     const user = await User.findById(req.user.id).select("-password");
-//     res.json(user);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: "Profile fetch failed" });
-//   }
-// });
-
-// export default authRouter;
-
-
-
-
-
-
-
-
 
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { User } from "../models/user.js";
+
+
 
 dotenv.config();
 const authRouter = express.Router();
